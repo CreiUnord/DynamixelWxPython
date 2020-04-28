78c1fce9b7c4c5c26a85951ad152a319b0ace6b7
CreiUnord <crei@unord.dk>
2020-04-28 21:41:59 +0200

    html index ny

--- EmuControl.py
+++ EmuControl.py
@@ -12,26 +12,32 @@ class Emu():
         self.sc.flush()
 
     def scanUnits(self):
+        """Scanner dynamixel motorer og returnere deres id'er i en liste"""
         ids = self.sc.scan()
         return ids
 
     def readDxl(self,ID):
+        """Printer oplysninger motoren med ID"""
         self.sc.flush()
         self.sc.pretty_print_control_table(ID)
 
     def jointMode(self,ID):
+        """Skifter motoren med ID til joint mode"""
         self.sc.set_cw_angle_limit(ID,0,False)
         self.sc.set_ccw_angle_limit(ID,1023,False)
 
     def wheelMode(self,ID):
+        """Skifter motoren med ID til wheelmode"""
         self.sc.set_ccw_angle_limit(ID,0,False)
         self.sc.set_cw_angle_limit(ID,0,False)
 
     def moveJoint(self, ID, position):
+        """Flytter motoren med ID til position"""
         self.sc.goto(ID, position, speed=512, degrees=True)
         time.sleep(1)
 
     def moveWheel(self,ID, speed):
+        """Starter en motor i wheelmode med hastigheden speed"""
         if speed < 0:
             if speed < -1024:
                 speed = 2047
@@ -44,8 +50,10 @@ class Emu():
         self.sc.goto(ID, 0, int(speed), degrees=False)
 
     def stop(self):
+        """Lukker forbindelsen gennem USB-porten til dynamixlerne"""
         self.sc.close()
 
     def getPos(self, ID):
+        """Returnere motoren med ID's position"""
         position = self.sc.get_present_position(ID, True)
         return position
